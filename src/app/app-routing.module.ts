f3e1e5a6ff2ebba5d01cd31be31b01d2b777ffa7
joshaston <josh.nickleby@astontech.com>
2020-07-30 19:16:24 -0500

    Removed the unnecessary items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,10 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
-import {P5BasicExampleComponent} from './p5/p5-dashboard/p5-example-section/p5-basic-example/p5-basic-example.component'
-import {P5StructureExampleComponent} from './p5/p5-dashboard/p5-example-section/p5-structure-example/p5-structure-example.component'
 
 
 const routes: Routes = [
   {path: 'p5/dashboard', loadChildren: () => import('./p5/p5-dashboard/p5-dashboard.module').then(m => m.P5DashboardModule)},
-  {path: 'ml5/dashboard', loadChildren: () => import('./ml5/ml5-dashboard/ml5-dashboard.module').then(m => m.ML5DashboardModule)},
-  {
-    path: 'p5/examples/basic',
-    component: P5BasicExampleComponent,
-    outlet: 'exampleDisplay'
-  },
-  {
-    path: 'p5/examples/structure',
-    component: P5StructureExampleComponent,
-    outlet: 'exampleDisplay'
-  }
+  {path: 'ml5/dashboard', loadChildren: () => import('./ml5/ml5-dashboard/ml5-dashboard.module').then(m => m.ML5DashboardModule)}
 ]
 
 @NgModule({
--- src/app/p5/p5-dashboard/p5-example-section/p5-example-section.module.ts
+++ src/app/p5/p5-dashboard/p5-example-section/p5-example-section.module.ts
@@ -3,13 +3,11 @@ import {CommonModule} from '@angular/common'
 
 import {P5ExampleSectionRoutingModule} from './p5-example-section-routing.module'
 import {P5ExampleSectionComponent} from './p5-example-section.component'
-import {ClarityModule} from '@clr/angular';
-import { P5BasicExampleComponent } from './p5-basic-example/p5-basic-example.component';
-import { P5StructureExampleComponent } from './p5-structure-example/p5-structure-example.component'
+import {ClarityModule} from '@clr/angular'
 
 
 @NgModule({
-  declarations: [P5ExampleSectionComponent, P5BasicExampleComponent, P5StructureExampleComponent],
+  declarations: [P5ExampleSectionComponent],
   imports: [
     CommonModule,
     P5ExampleSectionRoutingModule,
