8c29211f3a67d0fb9688bc51c65e2b9f483bf3a9
joshaston <josh.nickleby@astontech.com>
2020-08-03 14:38:46 -0500

    clear the x dimensions

--- src/app/p5/p5-dashboard/p5-sandbox/p5-sandbox.component.ts
+++ src/app/p5/p5-dashboard/p5-sandbox/p5-sandbox.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {ApplicationService} from '../../../services/application-service/application.service'
 import {P5Container} from '../../../shared/classes'
-import {P5Sketch} from '../../../shared/interfaces'
 import {P5GraphBounds, P5LineGraph} from '../../../shared/classes/p5/p5-line-graph'
 
 @Component({
@@ -12,38 +11,32 @@ import {P5GraphBounds, P5LineGraph} from '../../../shared/classes/p5/p5-line-gra
 export class P5SandboxComponent implements OnInit {
 
   container = P5Container.default()
-
-  constructor(private applicationService: ApplicationService) {
-    applicationService.changeLocation('p5', 'sandbox')
-  }
-
   bounds: P5GraphBounds = {
     x: 100,
     y: 100,
     width: 800,
     height: 400
   }
-
   xAxisLabels = [
     '6/2/2019', '6/16/2019', '6/30/2019', '7/14/2019', '7/28/2019', '8/11/2019',
     '8/25/2019', '9/8/2019', '9/22/2019', '10/6/2019', '10/20/2019'
   ]
-
   yAxisLabels = [
     70, 75, 80, 85, 90, 95, 100, 105
   ]
-
   graph: P5LineGraph
+
+  constructor(private applicationService: ApplicationService) {
+    applicationService.changeLocation('p5', 'sandbox')
+  }
 
   ngOnInit(): void {
     this.container = new P5Container(s => {
       s.setup = () => {
         s.createCanvas(1000, 800)
-        // this.graph = new P5LineGraph(s, this.bounds, {x: this.xAxisLabels, y: this.yAxisLabels})
       }
 
       s.draw = () => {
-        // this.createExample(s)
         s.background(255)
         s.fill(255)
 
@@ -53,46 +46,5 @@ export class P5SandboxComponent implements OnInit {
         this.graph.displayGraphAndPositions()
       }
     }, 'graphs')
-  }
-
-  createExample(s: P5Sketch) {
-    s.background(255)
-    s.fill(255)
-
-    s.stroke(0)
-    this.createAxisX(s, this.bounds, this.xAxisLabels)
-    this.createAxisY(s, this.bounds, this.yAxisLabels)
-  }
-
-  createAxisX(s: P5Sketch, bounds: {x: number, y: number, width: number, height: number}, labels: any[]) {
-    const line = {start: bounds.x, end: bounds.x + bounds.width}
-    const labelInfo = {
-      position: line.start
-    }
-
-    s.line(bounds.x, bounds.height + bounds.y, bounds.x + bounds.width, bounds.height + bounds.y)
-
-    const division = bounds.width / (labels.length - 1)
-
-    labels.forEach(label => {
-      s.circle(labelInfo.position, bounds.height + bounds.y, 10)
-      labelInfo.position += division
-    })
-  }
-
-  createAxisY(s: P5Sketch, bounds: {x: number, y: number, width: number, height: number}, labels: any[]) {
-    const line = {start: bounds.y, end: bounds.height + bounds.y}
-    const labelInfo = {
-      position: line.end
-    }
-
-    s.line(bounds.x, line.start, bounds.x, line.end)
-
-    const divisions = bounds.height / (labels.length - 1)
-
-    labels.forEach(label => {
-      s.circle(bounds.x, labelInfo.position, 10)
-      labelInfo.position -= divisions
-    })
   }
 }
--- src/app/shared/classes/p5/p5-line-graph.ts
+++ src/app/shared/classes/p5/p5-line-graph.ts
@@ -14,80 +14,101 @@ export interface P5GraphLabel {
 }
 
 interface P5GraphDimension {
-  legend: {
+  axis: {
     division?: number
     labels: P5GraphLabel[]
   }
-
-  actual?: number
+  start?: number
+  end?: number
 }
 
 export class P5LineGraph {
-  x = {
-    legend: {
+  x: P5GraphDimension = {
+    axis: {
       division: null,
       labels: []
     },
-    actual: null
+    start: null,
+    end: null
   }
 
-  y = {
-    legend: {
+  y: P5GraphDimension = {
+    axis: {
       division: null,
       labels: []
     },
-    actual: null
+    start: null,
+    end: null
   }
 
   constructor(public s: P5Sketch,
               public bounds: P5GraphBounds,
               public labels?: {x: any[], y: any[]}) {
-    console.log('------', bounds)
-    this.y.actual = bounds.height + bounds.y
+
+    this.y.start = bounds.y
+    this.y.end = bounds.height + bounds.y
+    this.x.start = bounds.x
+    this.x.end = bounds.x + bounds.width
   }
 
   createAxisX() {
-    this.s.line(this.bounds.x, this.y.actual, this.bounds.x + this.bounds.width, this.y.actual)
+    this.s.line(this.x.start, this.y.end, this.x.end, this.y.end)
   }
 
+  /**
+   *  Splits up the line by amount of labels -- need to catch too many (need a buffer)
+   *
+   *  For each label it adds the x/y coordinates calculated based on the bounds.
+   */
   createLabelsForX() {
-    let position = this.bounds.x
+    let position = this.x.start
 
     const division = this.bounds.width / (this.labels.x.length - 1)
 
-    this.x.legend.division = division
+    this.x.axis.division = division
 
     this.labels.x.forEach(label => {
-      this.x.legend.labels.push({x: position, y: this.y.actual, content: label})
+      this.x.axis.labels.push({x: position, y: this.y.end, content: label})
       position += division
     })
   }
 
+  /**
+   *  For each of the labels add a circle to where they exist on the x axis
+   */
   displayPositionsOfLabelsForX() {
-    this.x.legend.labels.forEach(label => {
+    this.x.axis.labels.forEach(label => {
       this.s.circle(label.x, label.y, 10)
     })
   }
 
   createAxisY() {
-    this.s.line(this.bounds.x, this.bounds.y, this.bounds.x, this.y.actual)
+    this.s.line(this.x.start, this.y.start, this.x.start, this.y.end)
   }
 
+  /**
+   *  Splits up the line by amount of labels -- need to catch too many (need a buffer)
+   *
+   *  For each label it adds the x/y coordinates calculated based on the bounds.
+   */
   createLabelsForY() {
-    let position = this.y.actual
+    let position = this.y.end
 
     const division = this.bounds.height / (this.labels.y.length - 1)
 
-    this.y.legend.division = division
+    this.y.axis.division = division
 
     this.labels.y.forEach(label => {
-      this.y.legend.labels.push({x: this.bounds.x, y: position, content: label})
+      this.y.axis.labels.push({x: this.x.start, y: position, content: label})
       position -= division
     })
   }
 
+  /**
+   *  For each of the labels add a circle to where they exist on the y axis
+   */
   displayPositionOfLabelsForY() {
-    this.y.legend.labels.forEach(label => {
+    this.y.axis.labels.forEach(label => {
       this.s.circle(label.x, label.y, 10)
     })
   }
